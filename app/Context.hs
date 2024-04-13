566334f2f473ba773f331d99ff4edd75038ab2f8
Matthew Schneider <panat.matthew@gmail.com>
2024-04-13 18:53:46 -0400

    Sharing what I have to my other computer.
    I am going to rewrite the context functions, since I tried to parameterize it
    for its two use cases (type checker and evaluator) and it is a complete mess.
    Perhaps I am going to turn it into two types or come up with an even better way
    to parameterize things.

--- app/Context.hs
+++ app/Context.hs
@@ -12,7 +12,12 @@ type TypeDict = Dict Type
 type FuncTypeDict = Dict ([Type], Type)
 
 data Context a f =
-  Context (Dict f) (Dict a) (Dict a) -- FUNCTIONS, states, locals
+  Context
+    { funcDict :: (Dict f)
+    , typeDict :: (Dict f)
+    , mstateDict :: (Dict a)
+    , localDict :: (Dict a)
+    }
   deriving (Show)
 
 type TypeContext = Context Type ([Type], Type)
@@ -29,36 +34,45 @@ funcToPair :: Func -> (Id, ([Type], Type))
 funcToPair (Func id ps t _) = (id, (map (\(Param _ t) -> t) ps, t))
 
 stateContext :: [MState] -> TypeContext
-stateContext ms = Context [] (stateToDict ms) []
+stateContext ms =
+  Context
+    {funcDict = [], typeDict = [], mstateDict = stateToDict ms, localDict = []}
 
 funcStateContext :: [Func] -> [MState] -> TypeContext
-funcStateContext fs ms = Context (funcsToDict fs) (stateToDict ms) []
+funcStateContext fs ms =
+  Context
+    { funcDict = funcsToDict fs
+    , typeDict = []
+    , mstateDict = stateToDict ms
+    , localDict = []
+    }
 
 emptyContext :: Context a f
-emptyContext = Context [] [] []
+emptyContext =
+  Context {funcDict = [], typeDict = [], mstateDict = [], localDict = []}
 
 -- type context operators
 setLocalContext :: [Param] -> TypeContext -> TypeContext
-setLocalContext ps (Context fs ms _) = Context fs ms ls
+setLocalContext ps (Context fs ts ms _) = Context fs ts ms ls
   where
     ls = map pair ps
     pair (Param id t) = (id, t)
 
 extendFuncContext :: Func -> TypeContext -> TypeContext
-extendFuncContext f (Context fs ms ls) = Context fs' ms ls
+extendFuncContext f (Context fs ts ms ls) = Context fs' ts ms ls
   where
     fs' = (funcToPair f) : fs
 
 -- extends the context with a local variable
 extendContext :: Id -> a -> Context a f -> Context a f
-extendContext id x (Context fs ms ls) = Context fs ((id, x) : ms) ls
+extendContext id x (Context fs ts ms ls) = Context fs ts ((id, x) : ms) ls
 
 {-
   If we assert that there are no duplicate keys in the three dictionaries, then
   it doesn't matter what order we check them.
 -}
 lookupContext :: Id -> Context a f -> Either String a
-lookupContext id (Context fs ms ls) =
+lookupContext id (Context _ _ ms ls) =
   case lookup id ms of
     Just x -> Right x
     Nothing ->
@@ -73,25 +87,25 @@ lookupContext' id ctx =
     Right v -> v
 
 lookupFuncContext :: Id -> Context a f -> Either String f
-lookupFuncContext id (Context fs _ _) =
+lookupFuncContext id (Context fs _ _ _) =
   case lookup id fs of
     Just x -> Right x
     Nothing -> Left $ "Lookup failed on function: " ++ show id
 
 lookupFuncContext' :: Id -> Context a f -> f
-lookupFuncContext' id (Context fs _ _) =
+lookupFuncContext' id (Context fs _ _ _) =
   case lookup id fs of
     Just f -> f
     Nothing ->
       error "Lookup of function failed. This should have been caught earlier."
 
 replaceContext :: Id -> a -> Context a f -> Context a f
-replaceContext id x (Context fs ms ls) =
+replaceContext id x (Context fs ts ms ls) =
   case replace id x ms of
-    Just ms' -> Context fs ms' ls
+    Just ms' -> Context fs ts ms' ls
     Nothing ->
       case replace id x ls of
-        Just ls' -> Context fs ms ls'
+        Just ls' -> Context fs ts ms ls'
         Nothing ->
           error "Replacement failed. This should have been caught earlier."
 
