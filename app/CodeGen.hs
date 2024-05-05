ea3d01f96ad9f76e3e8c76e68105df35ad649389
Matthew Schneider <panat.matthew@gmail.com>
2024-05-05 01:03:25 -0400

    adding palettes to compile time environment

--- app/CodeGen.hs
+++ app/CodeGen.hs
@@ -38,6 +38,7 @@ codeGenMode mode = do
                         vs)
                      0)
                 (modeTypes mode)
+          , palsDict = map (\(Palette n t _) -> (n, t)) (modePals mode)
           , gvarDict = []
           , mvarDict = map (\(MVar id t _) -> (id, t)) (modeVars mode)
           , lvarDict = []
